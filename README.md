cb2f5acb7fef5d5044a15abd19afa07b73e4b3f7
euirim <euirim@me.com>
2018-09-09 00:20:46 -0700

    Add initial files

---
+++ README.md
@@ -0,0 +1,9 @@
+# hustling-2018
+
+| Title | hustling-2018 |
+|-|-|
+| Developer    | [euirim](euirim@me.com) |
+| Link | [https://projects.chicagomaroon.com/article/2018/hustling-2018/](https://projects.chicagomaroon.com/article/2018/hustling-2018/) |
+
+
+©2018 The Chicago Maroon
---
+++ src/js/main-app.js
@@ -0,0 +1,82 @@
+import "../scss/main.scss";
+
+/*
+window.$('.icon-facebook').click((e) => {
+  e.preventDefault();
+  const uri = encodeURIComponent(window.location.href);
+  window.open(`https://www.facebook.com/sharer/sharer.php?u=${uri}`);
+});
+
+
+window.$('.icon-twitter').click((e) => {
+  e.preventDefault();
+  const uri = window.location.href;
+  const status = encodeURIComponent(`${window.tweetText} ${uri}`);
+  window.open(`https://twitter.com/intent/tweet?text=${status}`);
+});
+*/
+
+// prevent widows
+// let widowElements = [].slice.call(document.getElementsByClassName('no-widows'));
+let elements = document.querySelectorAll('.no-widows');
+Array.prototype.forEach.call(elements, function (el, i) {
+  let wordArray = el.textContent.trim().split(' ');
+  if (wordArray.length > 1) {
+    wordArray[wordArray.length - 2] += '&nbsp;' + wordArray[wordArray.length - 1];
+    wordArray.pop();
+    el.innerHTML = wordArray.join(' ');
+  }
+});
+
+// article card carousel
+$(document).ready(function () {
+  $('.article-card-carousel').slick({
+    infinite: true,
+    slidesToShow: 5,
+
+    centerMode: true,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    nextArrow: false,
+    prevArrow: false,
+    speed: 1500,
+    responsive: [{
+      breakpoint: 1800,
+      settings: {
+        slidesToShow: 4,
+      }
+    },{
+        breakpoint: 1400,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 500,
+        settings: {
+          slidesToShow: 1,
+          centerMode: false,
+        },
+      }
+    ],
+  });
+});
+
+/*
+console.log(widowElements.length);
+widowElements.map(function () {
+  var wordArray = $(this).text().split(' ');
+  if (wordArray.length > 1) {
+    wordArray[wordArray.length - 2] += '&nbsp;' + wordArray[wordArray.length - 1];
+    wordArray.pop();
+    $(this).html(wordArray.join(' '));
+  }
+});
+*/
